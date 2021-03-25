8543f69f42ac9eb44ef12ae639f2f997cd99f630
pgoggles <praneeth.gogineni@gmail.com>
2021-03-24 17:24:05 -0700

    Implemented Action Dispatchers: changeVideo, videoList, handleVideoSearch

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,16 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+  return () => {
+    searchYouTube({ key: YOUTUBE_API_KEY, query: q }, (data) => {
+      changeVideo(data[0]);
+      changeVideoList(data);
+    });
+  };
+
+
 };
 
 export default handleVideoSearch;
